6bfdbc890e570d6f89656bcccdb70cfd2bac2c2b
vert <vert@vert.com>
2020-12-21 03:42:31 -0500

    clean up

--- AddOns/CombatAlerts/GetFormattedAbilityName.lua
+++
@@ -1,6 +0,0 @@
-if not GetFormattedAbilityName then
-	GetFormattedAbilityName = function( abilityId )
-		local name = GetAbilityName(abilityId)
-		return(name ~= "" and LocalizeString("<<t:1>>", name) or string.format("[#%d]", abilityId))
-	end
-end
