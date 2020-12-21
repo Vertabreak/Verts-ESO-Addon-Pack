7d7392d038e90098449ec2bc25fe2ec7773fff21
vert <vert@vert.com>
2020-12-21 04:04:13 -0500

    clean up

--- AddOns/VotansSettingsMenu/lang/fr.lua
+++
@@ -1,4 +0,0 @@
-SafeAddString(SI_VOTANS_SETTINGS_MENU_SHOW_BUTTON, "Afficher le bouton Extensions dans le menu")
-SafeAddString(SI_VOTANS_SETTINGS_MENU_MEM_USAGE, "Utilisation de la mémoire LUA")
-SafeAddString(SI_VOTANS_SETTINGS_MENU_USED_LIBRARIES, "Librairies employées")
-SafeAddString(SI_VOTANS_SETTINGS_MENU_MEM_TOO_LOW, "Le seuil d'alerte de mémoire réduite est plus basse qu'en usage normal!")
